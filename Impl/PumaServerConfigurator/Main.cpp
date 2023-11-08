80225f215d35baaea99add76402e4bb24179c965
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-11-08 03:25:45 +0000

    Qml for qt6 corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@7875 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/PumaServerConfigurator/Main.cpp
+++ Impl/PumaServerConfigurator/Main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 	qmlRegisterModule("QtQuick.Dialogs", 1, 3);
 #else
 	qmlRegisterModule("QtQuick.Dialogs", 6, 2);
-	qmlRegisterModule("Qt5Compat.GraphicalEffects", 1, 0);
+	qmlRegisterModule("Qt5Compat.GraphicalEffects", 6, 0);
 #endif
 
 	CPumaServerConfigurator instance;
