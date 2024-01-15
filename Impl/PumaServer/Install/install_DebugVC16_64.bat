c2aa722d3395bd83144360ff7a18a6dc77824a4e
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-01-15 05:04:24 +0000

    Puma partitura corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@8420 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/PumaServer/Install/install_DebugVC16_64.bat
+++ Impl/PumaServer/Install/install_DebugVC16_64.bat
@@ -1,4 +1,4 @@
 @echo on
-set PUMA_BUILD_DIR=..\..\..\Bin\Debug_Qt5_VC16_x64
+set PUMA_BUILD_DIR=..\..\..\Bin\Debug_Qt6_VC16_x64
 iscc PumaServer.iss
 pause
--- Impl/PumaServer/Install/install_ReleaseVC16_64.bat
+++ Impl/PumaServer/Install/install_ReleaseVC16_64.bat
@@ -1,5 +1,5 @@
 @echo on
-set PUMA_BUILD_DIR=..\..\..\Bin\Release_Qt5_VC16_x64
+set PUMA_BUILD_DIR=..\..\..\Bin\Release_Qt6_VC16_x64
 iscc PumaServer.iss
 pause
 
