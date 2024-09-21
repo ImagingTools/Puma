8ea282b6fdb4986ddfe6f802de21385c7b7c6bfd
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-09-21 04:51:46 +0000

    The user LDAP can be removed; Lisa bugs fixed; Base Class QML json parse corrected;
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@11213 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/PumaServerConfigurator/Main.cpp
+++ Impl/PumaServerConfigurator/Main.cpp
@@ -1,55 +1,15 @@
-// Qt includes
-#include <QtQml/QQmlEngine>
-#include <QtCore/QCoreApplication>
-#include <QtWidgets/QApplication>
-#include <QtQml>
-
-// ACF includes
-#include <ibase/IApplication.h>
-
 // ImtCore includes
+#include <imtbase/Init.h>
+
+// Puma includes
 #include <GeneratedFiles/PumaServerConfigurator/CPumaServerConfigurator.h>
-#include <imtbase/CTreeItemModel.h>
-#include <imtqml/CGqlModel.h>
-#include <imtqml/CRemoteFileController.h>
 
 
 int main(int argc, char *argv[])
 {
 	Q_INIT_RESOURCE(PumaServerConfigurator);
-	Q_INIT_RESOURCE(imtgui);
-	Q_INIT_RESOURCE(imtguiqml);
 	Q_INIT_RESOURCE(pumaqml);
-	Q_INIT_RESOURCE(imtguiTheme);
-
-	Q_INIT_RESOURCE(imtstylecontrolsqml);
-	Q_INIT_RESOURCE(imtcontrolsqml);
-	Q_INIT_RESOURCE(imtguigqlqml);
-	Q_INIT_RESOURCE(imtauthguiqml);
-	Q_INIT_RESOURCE(imtcolguiqml);
-	Q_INIT_RESOURCE(imtdocguiqml);
-
-	qmlRegisterType<imtbase::CTreeItemModel>("Acf", 1, 0, "TreeItemModel");
-	qmlRegisterType<imtqml::CGqlModel>("Acf", 1, 0, "GqlModel");
-	qmlRegisterType<imtqml::CRemoteFileController>("Acf", 1, 0, "RemoteFileController");
-
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
-	qmlRegisterModule("QtGraphicalEffects", 1, 12);
-	qmlRegisterModule("QtGraphicalEffects", 1, 0);
-	qmlRegisterModule("QtQuick.Dialogs", 1, 3);
-#else
-	qmlRegisterModule("QtQuick.Dialogs", 6, 2);
-	qmlRegisterModule("Qt5Compat.GraphicalEffects", 6, 0);
-#endif
-
-	CPumaServerConfigurator instance;
-
-	ibase::IApplication* applicationPtr = instance.GetInterface<ibase::IApplication>();
-	if (applicationPtr != nullptr){
-		return applicationPtr->Execute(argc, argv);
-	}
-
-	return -1;
+	return Run<CPumaServerConfigurator, DefaultImtCoreQmlInitializer>(argc, argv);
 }
 
 
