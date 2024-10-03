c25c9a4b7d02b5b85726da6f539bddac38ffa876
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-10-03 04:12:35 +0000

    Code cleanup; Partitura corrected;
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@11314 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/PumaServerConfigurator/Main.cpp
+++ Impl/PumaServerConfigurator/Main.cpp
@@ -1,5 +1,6 @@
 // ImtCore includes
 #include <imtbase/Init.h>
+#include <imtqml/CQmlProcess.h>
 
 // Puma includes
 #include <GeneratedFiles/PumaServerConfigurator/CPumaServerConfigurator.h>
@@ -9,6 +10,7 @@ int main(int argc, char *argv[])
 {
 	Q_INIT_RESOURCE(PumaServerConfigurator);
 	Q_INIT_RESOURCE(pumaqml);
+	qmlRegisterType<imtqml::CQmlProcess>("imtqml", 1, 0, "Process");
 	return Run<CPumaServerConfigurator, DefaultImtCoreQmlInitializer>(argc, argv);
 }
 
