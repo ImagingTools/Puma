4283aeea47455f4d5db2540e755b79be7ba7608e
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-01-21 16:27:01 +0000

    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@12678 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/PumaServer/Install/install_ReleaseVC16_64.bat
+++ Impl/PumaServer/Install/install_ReleaseVC16_64.bat
@@ -4,6 +4,6 @@ set OUTPUT_FILE=tempFile.txt
 set /p APP_VERSION=< %OUTPUT_FILE% 
 IF EXIST %OUTPUT_FILE% DEL /F %OUTPUT_FILE%
 
-set PUMA_BUILD_DIR=..\..\..\Bin\Release_Qt6_VC16_x64
+set PUMA_BUILD_DIR=..\..\..\Bin\Release_Qt6_VC17_x64
 iscc PumaServer.iss
 
