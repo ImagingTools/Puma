1280234a53e32a4414b9f0d1b71ba6688ef27aac
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-10-21 09:24:32 +0000

    PumaClient added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@15135 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Impl/PumaClient/Main.cpp
@@ -0,0 +1,49 @@
+// ImtCore includes
+#include <imtbase/Init.h>
+
+// PumaClient includes
+#include <GeneratedFiles/PumaClient/CPumaClient.h>
+
+// #include <CAdministrationViewWidget.h>
+// #include <AuthServerSdk/AuthServerSdk.h>
+// #include <AuthClientSdk/AuthClientSdk.h>
+#include <QLayout>
+
+
+int main(int argc, char *argv[])
+{
+	Q_INIT_RESOURCE(PumaClient);
+
+	QApplication app(argc, argv);
+
+	// AuthServerSdk::CAuthorizableServer server;
+	// server.SetDatabaseSettings("localhost", 5432, "test", "postgres", "root");
+	// server.SetPumaConnectionParam("localhost", 7788, 8788);
+	// server.SetProductId("Test");
+	// server.SetFeaturesFilePath("D:\\ImagingTools\\ITDevelopment\\Puma\\Impl\\PumaClient\\TestFeatures.xml");
+
+	// server.Start(7777, 8888);
+
+	// AuthClientSdk::CAuthorizationController authorizationController;
+	
+	// AuthClientSdk::Login loginData;
+	// authorizationController.SetProductId("Test");
+
+	// bool ok = authorizationController.Login("su", "1", loginData);
+	// if (ok){
+	// 	qDebug() << "Login successfuly" << loginData.accessToken;
+	// }
+	// else{
+	// 	qDebug() << "Login failed";
+	// }
+
+	// AuthClientSdk::CAdministrationViewWidget loginWidget;
+	// loginWidget.SetConnectionParam("localhost", 7777, 8888);
+	// loginWidget.SetLoginParam(loginData);
+	// loginWidget.setWindowTitle("Login Widget");
+	// loginWidget.resize(1000, 1000);
+	// loginWidget.show();
+
+	return app.exec();
+}
+
