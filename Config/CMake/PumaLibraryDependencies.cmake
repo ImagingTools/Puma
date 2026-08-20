ee9d4d5c77455616b390999b7e54c6ba8c8b22a6
Sergey Zastrovnyh <sergeizastrovnyh@yandex.com>
2026-08-20 16:04:49 +0600

    Refactor CMake target link declarations to use explicit PUBLIC/PRIVATE scopes, enhancing clarity and consistency in dependency management.

--- Config/CMake/PumaLibraryDependencies.cmake
+++ Config/CMake/PumaLibraryDependencies.cmake
@@ -20,19 +20,18 @@
 # so those are not repeated). Do not add a dependency that is already reachable
 # through another listed target.
 #
-# Puma uses keyword target_link_libraries() signatures (PUBLIC/PRIVATE/
-# INTERFACE) consistently via ACF_LIBRARY_LINK_SCOPE.
+# Puma uses explicit PUBLIC/PRIVATE/INTERFACE link scopes.
 #
 # Included once, centrally, from Build/CMake/CMakeLists.txt after all library
 # targets have been created.
 # ---------------------------------------------------------------------------
 
 if(QT_VERSION_MAJOR EQUAL 6)
-	acf_declare_target_dependencies(pumaqml LINK_SCOPE ${ACF_LIBRARY_LINK_SCOPE}
+	acf_declare_target_dependencies(pumaqml LINK_SCOPE PUBLIC
 		Qt${QT_VERSION_MAJOR}::Core5Compat
 	)
 endif()
 
-acf_declare_target_dependencies(pumatest LINK_SCOPE ${ACF_APPLICATION_LINK_SCOPE}
+acf_declare_target_dependencies(pumatest LINK_SCOPE PRIVATE
 	AuthClientSdk
 )
